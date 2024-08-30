c3f3abdf8011df465839caa699338115f5a6f097
Mahmoud <mahmoudhatim55@gmial.com>
2024-08-30 17:48:45 +0200

    rotation matrix

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
+#include <cstdio>
 #include <iostream>
+#include <vector>
 #include "rot_mat.hpp"
+
 
 //print the matrix (for testing)
 void printMatrix(const Matrix3x3& matrix){
@@ -24,7 +27,32 @@ int main() {
 	printMatrix(Rz(theta));
 
 	return 0;
+        
+	//Create another file for reading the files (load the files 
+	//Add the las2txt command here to automatically witch .las/.laz files to .xyz
+	//
+	
 
-}
+        std::vector<Point3D> points;
+	Point3D tempPt;
+
+	//Reading points from ASCII formated file
+	FILE* inFile = fopen("filename.xyz", "r");
+	if (inFile) {
+		while (fscanf(inFile, "%lf %lf %lf\n", &tempPt.x, &tempPt.y, &tempPt.z)=3)
+		{
+			points.push_back(tempPt);
+		}
+		fclose(inFile);
+	}
+	else {
+		std::cout << "Error in loading the .xyz file" << std::endl;
+		return 1;
+	}
+
+	//Parameters!
+	//maybe for the angles and projections here
+	
+	}
 
 
